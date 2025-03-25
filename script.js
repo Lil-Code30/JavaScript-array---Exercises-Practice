12b84f3a6cd466a978139b5ae6412d9458c62364
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-03-24 20:18:43 -0400

    JavaScript-Arrays - Exercises

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ let arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
 
 console.log(sortArray(arr1));
 
-*/
+
 
 // 8 Write a JavaScript program to find the most frequent item in an array.
 let obj = {};
@@ -120,7 +120,7 @@ function mostFrequent(arr){
         obj[element]++;
     }
    });
-   
+
    // verify the max key, value pair
    let max = 0;
    let maxElement;
@@ -137,3 +137,98 @@ function mostFrequent(arr){
 
 let arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 console.log(mostFrequent(arr1));
+
+
+// 9 Write a JavaScript program that accepts a string as input and swaps the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+function swapCase(str){
+    strToArr = str.split(' ');
+    result = strToArr.map(word => {
+        return word.split('').map(letter => {
+            return letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase();
+        }).join('');
+    })
+    return result.join(' ');
+}
+
+str1 = "T";
+console.log(swapCase(str1));
+
+
+
+// 10 Write a JavaScript program that prints the elements of the following array.
+
+// Note : Use nested for loops.
+
+// Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// Sample Output :
+
+// "row 0"
+// " 1"
+// " 2"
+// " 1"
+// " 24"
+// "row 1"
+// ------
+// ------
+
+let arr1 = [1, [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+let arr2 = [[8, 1, 3, 4], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+function printArrayElements(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            console.log(`row ${i}`);
+            for(let j = 0; j < arr[i].length; j++){
+                console.log(arr[i][j]);
+            }
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+printArrayElements(arr1);
+console.log("----------------------------");
+printArrayElements(arr2);
+
+
+
+// 11. Write a JavaScript program to find the sum of squares of a numerical vector.
+
+let arr1 = [1, 2, 3, 4, 5];
+function sumOfSquares(arr){
+    let sum = 0;
+
+    let squares = arr.map(num => num*num);
+
+    for(let i=0; i < squares.length; i++){
+        sum =  sum + squares[i];
+    }
+
+    return sum;
+}
+
+console.log(sumOfSquares(arr1));
+
+
+// 12. Write a JavaScript program to compute the sum and product of an array of integers.
+
+let arr1 = [1, 2, 3, 4, 5,6];
+function sumAndProduct(arr){
+    let sum = 0;
+    let product = 1;
+
+    for(let i=0; i < arr.length; i++){
+        sum += arr[i];
+        product *= arr[i];
+    }
+
+    return `Sum: ${sum} and Product: ${product}`;
+}
+
+console.log(sumAndProduct(arr1));
+
+*/
+
+// 13 Add Items to Array
+// Write a JavaScript program to add items to a blank array and display them.
